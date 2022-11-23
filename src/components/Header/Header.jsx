9336f9aaf0b0d039f98b22a1b93b966ef2b782ac
3egle <samuelternenge01@gmail.com>
2022-11-23 11:03:38 +0100

    all

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,7 +3,7 @@ import { useState } from "react";
 import { CgMenuGridO } from "react-icons/cg";
 
 import Navbar from "./Navbar";
-import NavbarMobile from "./NavbarMobile";
+
 const Header = () => {
   const [sidebar, setSidebar] = useState(false);
 
@@ -15,21 +15,7 @@ const Header = () => {
       setSidebar(true);
     }
   }
-  return (
-    <div className="relative w-full p-3 flex items-center ">
-      <header className="flex items-center justify-between text-center w-full">
-        <div className="text-[25px] cursor-pointer">
-          <CgMenuGridO onClick={toggleSidebar}></CgMenuGridO>{" "}
-        </div>
-        <p>District Programmer</p>
-        <div className="capitalize border rounded w-[45px] flex items-center justify-center text-center bg-pink-600 hover:bg-green-900">
-          join
-        </div>
-      </header>
-      <Navbar sidebar={sidebar} />
-      <NavbarMobile sidebar={sidebar} setSidebar={setSidebar} />
-    </div>
-  );
+  return <div className="relative w-full p-3 flex items-center "></div>;
 };
 
 export default Header;
--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,1 @@
 export { default as Header } from "./Header/Header";
-export { default as Hero } from "./Hero/Hero";
-export { default as SocialLinks } from "./SocialLinks";
-export { default as Footer } from "./Footer/Footer";
